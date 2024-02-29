753df7334cd51df933cf4be6aa7869f862c35f03
António Oliveira <antonioliveira@protonmail.com>
2024-02-29 17:21:05 +0000

    env test train test

--- src/grl/env_test.py
+++ src/grl/env_test.py
@@ -2,29 +2,49 @@ import matplotlib
 import matplotlib.pyplot as plt
 import grid2op
 from grid2op.Agent import RandomAgent
+from lightsim2grid import LightSimBackend
 
 
 # Create the environment
-env_name = ("./data_grid2op/l2rpn_case14_sandbox/")
-env = grid2op.make(env_name)
+env_name = "./data_grid2op/l2rpn_idf_2023/"
+backend_class = LightSimBackend
+env = grid2op.make(env_name, backend=backend_class())
+
+env.seed(0)  # for reproducible experiments
+episode_count = 5  # i want to make lots of episode
+
+
+# total number of episode
+total_episode = len(env.chronics_handler.subpaths)
 
 # Initialize variables
-myagent = RandomAgent(env.action_space)
+agent = RandomAgent(env.action_space)
 obs = env.reset()
-reward = env.reward_range[0] 
+reward = 0
 done = False
-nb_step = 0
+total_reward = 0
 
 update_interval = 1                             # Update interval
-max_iter = 40                                   # Maximum number of iterations
 
+# and now the loop starts
+for i in range(episode_count):
 
-while not done:
-    fig = env.render()                          # render the environment
-    plt.show()  # Show the plot after each iteration
-    act = myagent.act(obs, reward, done)        # call the act method of the agent
-    obs, reward, done, info = env.step(act)     # take the action and update the environment
-    nb_step += 1                                # update the number of steps
+    ###################################
+    if i % total_episode == 0:
+        # I shuffle each time i need to
+        env.chronics_handler.shuffle()
+    ###################################
 
-    if nb_step >= max_iter:
-        break
+    obs = env.reset()
+    # now play the episode as usual
+    while True:
+        fig = env.render()  # render the environment
+        plt.show()  # Show the plot after each iteration
+        action = agent.act(obs, reward, done)
+        obs, reward, done, info = env.step(action)
+        print("Cost per MW:" + str(env.gen_cost_per_MW))
+        print("Storage chargeW:" + str(env.storage_charge))
+        total_reward += reward
+        if done:
+            # in this case the episode is over
+            break
---
+++ src/grl/train_test_val.py
@@ -0,0 +1,13 @@
+import grid2op
+env_name = "l2rpn_case14_sandbox"  # or any other...
+env = grid2op.make(env_name)
+
+# extract 1% of the "chronics" to be used in the validation environment. The other 99% will
+# be used for test
+nm_env_train, nm_env_val, nm_env_test = env.train_val_split_random(pct_val=1, add_for_test="test", pct_test=1)
+
+# and now you can use the training set only to train your agent:
+print(f"The name of the training environment is \"{nm_env_train}\"")
+print(f"The name of the validation environment is \"{nm_env_val}\"")
+print(f"The name of the test environment is \"{nm_env_test}\"")
+env_train = grid2op.make(nm_env_train)
